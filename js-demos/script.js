d3a1a22edbb8d4cab17513d04866f040bd018b6c
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-13 17:15:30 +0530

    js demos

--- js-demos/script.js
+++ js-demos/script.js
@@ -33,9 +33,57 @@
 // console.log(num3);
 // console.log(num4);
 
-console.log("abc");
-console.log('abc');
-console.log(`abc`);
+// console.log("abc");
+// console.log('abc');
+// console.log(`abc`);
+
+
+// datatypes identification 
+
+// let num = 10;
+// console.log(num);
+// console.log(typeof (num));
+// num = `abc`;
+// console.log(num);
+// console.log(typeof (num));
+// num = true;
+// console.log(num);
+// console.log(typeof (num));
+
+// let num = 10;
+// console.log(num);
+// num = 20;
+// console.log(num);
+// const num2 = 10;
+// console.log(num2);
+// num2 = 20;
+// console.log(num2);
+
+// let myMarks = [10, 20, 30]; // array 
+// console.log(myMarks);
+// console.log(myMarks[0]);
+
+let arr = [10, 20.5, `abc`, false];
+console.log(arr[3]);
+
+let obj = {}; // proper js object 
+
+let empData = {
+    employeeId: 101,
+    firstName: `Sonu`,
+    salary: 50000.25,
+    address: { street: `ECIL Road`, city: `Hyderabad`, pin: 123456 },
+    phones: [9876543210, 7418529630],
+    isMarried: false
+};
+console.log(empData);
+console.log(empData.address.street);
+console.log(empData.phones[1]);
+
+
+
+
+
 
 
 
