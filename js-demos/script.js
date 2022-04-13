c6d44e3eaab8927a37d1a0ba8bb6e8f6c224b61f
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-13 16:48:02 +0530

    js demos

--- js-demos/script.js
+++ js-demos/script.js
@@ -1,3 +1,46 @@
 
-console.log("js demo"); // sop java 
-console.log(10 + 20);
+// JS engines - 
+// chrome - v8 
+
+// console.log("js demo"); // sop java 
+// console.log(10 + 20);
+
+
+// java 
+// int num = 10;
+// sop(num);
+// num = "abc";
+// sop(num);
+
+// js variable declaration 
+
+// let num = 10;
+// console.log(num);
+// num = "abc";
+// console.log(num);
+// num = true;
+// console.log(num);
+
+// datatypes in JS 
+// number, string, boolean 
+
+// num = 10; // don't use 
+// var num2 = 20; // don't use 
+// let num3 = 30; // use for normal variables 
+// const num4 = 40; // use for constants = java final 
+// console.log(num);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+console.log("abc");
+console.log('abc');
+console.log(`abc`);
+
+
+
+
+
+
+
+
