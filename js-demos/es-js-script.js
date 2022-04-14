29c2c5e76ce68f96bc6dc3ea26e5bc286eba2b32
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-14 12:50:14 +0530

    js callbacks

---
+++ js-demos/es-js-script.js
@@ -0,0 +1,46 @@
+// // // old JS 
+// // function fun() {
+// //     console.log(`fun function`);
+// // }
+
+// // // ECMAScript, ES complient, new JS 
+// // let fun2 = () => {
+// //     console.log(`fun2 function`);
+// // }
+
+// // fun();
+// // fun2();
+
+// let num;
+// const num2 = 10;
+
+// class Employee {
+
+//     employeeId;
+//     firstName;
+//     salary;
+
+//     constructor() {
+
+//     }
+
+//     someMethod = () => {
+//         console.log(this.firstName);
+//     }
+
+// }
+
+// // // object of a class
+// let emp = new Employee();
+// emp.employeeId = 101;
+// console.log(emp.employeeId);
+// emp.someMethod();
+
+
+// // // proper JS object
+// // let emp2 = {};
+
+
+
+
+
