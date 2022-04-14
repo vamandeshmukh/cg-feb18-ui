af45d103adae0a3f383dcc5c29af60af9c6d9166
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-14 10:45:16 +0530

    js callbacks

--- js-demos/script.js
+++ js-demos/script.js
@@ -63,22 +63,159 @@
 // console.log(myMarks);
 // console.log(myMarks[0]);
 
-let arr = [10, 20.5, `abc`, false];
-console.log(arr[3]);
+// let arr = [10, 20.5, `abc`, false];
+// console.log(arr[3]);
 
-let obj = {}; // proper js object 
+// let obj = {}; // proper js object 
 
-let empData = {
-    employeeId: 101,
-    firstName: `Sonu`,
-    salary: 50000.25,
-    address: { street: `ECIL Road`, city: `Hyderabad`, pin: 123456 },
-    phones: [9876543210, 7418529630],
-    isMarried: false
-};
-console.log(empData);
-console.log(empData.address.street);
-console.log(empData.phones[1]);
+// let empData = {
+//     employeeId: 101,
+//     firstName: `Sonu`,
+//     salary: 50000.25,
+//     address: { street: `ECIL Road`, city: `Hyderabad`, pin: 123456 },
+//     phones: [9876543210, 7418529630],
+//     isMarried: false
+// };
+// console.log(empData);
+// console.log(empData.address.street);
+// console.log(empData.phones[1]);
+// console.log(typeof(empData));
+
+
+
+// null
+// NaN 
+// undefined 
+
+// let v1 = 10;
+// let v2 = `abc`;
+// let v3 = false;
+
+// // truthy, falsy 
+// // falsy : false, 0, NaN, undefined, null
+// // truthy : everything else 
+
+// let userInput = `abc`;
+
+// // if (/*userInput has a value*/) {
+// if (userInput == 'abc') {
+//     // code 
+// }
+// else {
+//     // code 
+// }
+
+// let v1 = 10;
+// let v2 = `10`;
+
+// if (v1 === v2) {
+//     console.log(`true`);
+// }
+// else {
+//     console.log(`false`);
+// }
+
+
+// let arr1 = [10, 20, 30];
+// let arr2 = [40, 50, 60];
+// console.log(arr1);
+// console.log(arr2);
+
+// let arr3 = [...arr1, ...arr2]
+// // let arr3 = [arr1, arr2];
+// console.log(arr3);
+// console.log(typeof (arr3));
+
+// functions in JS 
+// function == method == hook == routine == subroutine == ??
+
+// class Delete {
+
+// }
+
+// function fun() {
+//     console.log(`fun function`);
+// }
+
+// function fun2() {
+//     console.log(`fun2 function`);
+//     return 10;
+// }
+
+// console.log(fun());
+// console.log(fun2());
+
+// function fun3(arg1, arg2) {
+//     console.log(arg1 + arg2);
+//     return arg1 + arg2;
+// }
+
+// console.log(fun3());
+// console.log(fun3(10));
+// console.log(fun3(10, 20));
+// console.log(fun3(10, 20, 30));
+
+// arrrow functions 
+//   =>       ->     
+
+// let fun4 = () => {
+
+// }
+
+// let fun4 = () => {
+//     console.log(`fun4 function`);
+// }
+// fun4();
+
+// let fun5 = () => console.log(`fun5 function`);
+
+// fun5();
+
+// let fun6 = v1 => console.log(v1);
+// // let fun6 = (v1) => console.log(v1);
+// // let fun6 = v1 => {console.log(v1)};
+// // let fun6 = (v1) => {console.log(v1)};
+
+// fun6(10);
+
+
+// asynchronous JS - non-blocking JS 
+
+// console.log('one');
+
+// // setTimeout(arg1, arg2);
+// // setTimeout(callback function, MS);
+// // setTimeout(() => { }, 2000);
+
+// setTimeout(() => {
+//     console.log('two');
+// }, 2000);
+
+// console.log('three');
+
+// // async issue 
+// let fun = () => {
+//     setTimeout(() => {
+//         return { message: `fun function` };
+//     }, 2000);
+// }
+// console.log(`start`);
+// let getFun = fun();
+// console.log(getFun.message);
+
+// async issue - solution 1 callback 
+
+let fun = (arg) => {
+    setTimeout(() => {
+        arg({ message: `fun function` });
+    }, 2000);
+}
+
+console.log(`start`);
+
+fun((abc) => {
+    console.log(abc.message);
+});
 
 
 
